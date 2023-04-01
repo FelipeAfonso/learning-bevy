befd5a54597674246b7cc29405d43b295905d8c4
Felipe Munhoz Afonso <fmunhozafonso@gmail.com>
2023-04-01 00:51:04 -0300

    feat: rotating cube

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,67 @@
 use bevy::prelude::*;
-
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
+mod greeter;
 
 #[derive(Resource)]
-struct GreetTimer(Timer);
+struct RotateTimer(Timer);
 
-pub struct HelloPlugin;
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
-    }
+#[derive(Component)]
+pub struct CubeEntity;
+
+fn main() {
+    App::new()
+        .insert_resource(RotateTimer(Timer::from_seconds(0.05, TimerMode::Repeating)))
+        .add_plugins(DefaultPlugins)
+        //.add_plugin(greeter::HelloPlugin)
+        .add_startup_system(setup)
+        .add_system(rotate_cube)
+        .run();
 }
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Ozzy Osbourne".to_string())));
-    commands.spawn((Person, Name("Dio".to_string())));
-    commands.spawn((Person, Name("Rob Halford".to_string())));
-}
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+fn rotate_cube(
+    time: Res<Time>,
+    mut timer: ResMut<RotateTimer>,
+    mut query: Query<&mut Transform, With<CubeEntity>>,
+) {
     if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("Hello {}!", name.0)
+        for mut cube in &mut query {
+            cube.rotate_y(10.0 * time.delta_seconds())
         }
     }
 }
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
-        .run();
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(shape::Plane::from_size(5.0).into()),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+
+    commands.spawn((
+        CubeEntity,
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..default()
+        },
+    ));
+
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
