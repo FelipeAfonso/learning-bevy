fe1c1baf09ada72cba3fda979129b32065c484b4
Felipe Munhoz Afonso <fmunhozafonso@gmail.com>
2023-11-25 20:36:10 -0300

    feat: added web collision

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -8,35 +8,52 @@ use crate::entities::{EnemyEntity, PlayerAttached, PlayerEntity};
 pub struct GamePlugin;
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(detect_intersection);
+        app.add_system(detect_intersection_player);
     }
 }
 
-pub fn detect_intersection(
+pub fn detect_intersection_player(
     mut commands: Commands,
     enemy_query: Query<(&Transform, &Sprite, Entity), With<EnemyEntity>>,
     player_query: Query<
         (&Transform, &Sprite, Entity),
         (With<PlayerEntity>, Without<PlayerAttached>),
     >,
+    web_query: Query<(&Transform, &Sprite, Entity), (With<PlayerEntity>, With<PlayerAttached>)>,
 ) {
     for player in player_query.iter() {
         let player_pos = player.0.translation;
         let player_size = player.1.custom_size.unwrap_or_default();
         let player_entity = player.2;
-        for enemy in enemy_query.iter() {
-            let enemy_pos = enemy.0.translation;
-            let enemy_size = enemy.1.custom_size.unwrap_or_default();
-            let enemy_entity = enemy.2;
-            match collide(player_pos, player_size, enemy_pos, enemy_size) {
-                Some(Collision::Bottom) => {
-                    commands.entity(player_entity).despawn();
-                }
-                Some(_collision) => {
-                    commands.entity(enemy_entity).despawn();
-                }
-                _ => {}
-            };
+        for web in web_query.iter() {
+            let web_pos = web.0.translation;
+            let web_size = web.1.custom_size.unwrap_or_default();
+            let web_entity = web.2;
+            for enemy in enemy_query.iter() {
+                let enemy_pos = enemy.0.translation;
+                let enemy_size = enemy.1.custom_size.unwrap_or_default();
+                let enemy_entity = enemy.2;
+                match collide(player_pos, player_size, enemy_pos, enemy_size) {
+                    Some(Collision::Bottom) => {
+                        commands.entity(player_entity).despawn();
+                        commands.entity(web_entity).despawn();
+                        //TODO: toggle game over
+                    }
+                    Some(_collision) => {
+                        commands.entity(enemy_entity).despawn();
+                        //TODO: add score / regen energy
+                    }
+                    _ => {}
+                };
+                match collide(web_pos, web_size, enemy_pos, enemy_size) {
+                    Some(_collision) => {
+                        commands.entity(web_entity).despawn();
+                        commands.entity(player_entity).despawn();
+                        //TODO: toggle game over
+                    }
+                    _ => {}
+                };
+            }
         }
     }
 }
