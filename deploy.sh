a080dc8adb5453d2c859fa1a248d769237858d1c
Felipe Afonso <fmunhozafonso@gmail.com>
2024-07-04 12:18:55 -0300

    feat: added deployable version

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# build game
+cargo build --release --target wasm32-unknown-unknown
+# generate deployable wasm
+wasm-bindgen --no-typescript --target web --out-dir ./site/public/ --out-name "stupid-spider-game" ./target/wasm32-unknown-unknown/release/learning-bevy.wasm
+# copy assets into site
+cp ./assets/**/*.{png,ogg,ttf} ./site/public --parents
+
+# site building
+cd ./site
+pnpm build
