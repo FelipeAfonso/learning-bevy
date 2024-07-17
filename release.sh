6953c9b48606770b2c4ef749a860329638aac73d
Felipe Afonso <fmunhozafonso@gmail.com>
2024-07-16 21:23:33 -0300

    feat: streamlined release process

--- release.sh
+++ release.sh
@@ -6,8 +6,10 @@ cargo build --target=x86_64-pc-windows-gnu --release
 #move releases to release folder
 cp ./target/release/stupid-spooder-game ./release/stupid-spooder-game
 cp ./target/x86_64-pc-windows-gnu/release/stupid-spooder-game.exe ./release/stupid-spooder-game.exe
+cp ./assets/**/*.{png,mp3,ttf} ./release/ -r --parents
 # zip releases
 cd ./release
-zip stupid-spooder-game.zip stupid-spooder-game stupid-spooder-game.exe
+zip stupid-spooder-game-windows.zip stupid-spooder-game.exe assets -r
+zip stupid-spooder-game-linux.zip stupid-spooder-game assets -r
 cd ../
-mv ./release/stupid-spooder-game.zip ./
+mv ./release/stupid-spooder-game-*.zip ./
